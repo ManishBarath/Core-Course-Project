d2fab365fe5e34bf248fb3f21068c607d22f64ac
dhanushkrishnac7 <dhanushkrishnac.cse2023@gmail.com>
2025-02-06 11:43:02 +0530

    added database config

---
+++ Backend/database.js
@@ -0,0 +1,9 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+console.log(SUPABASE_URL)
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
+
+const supabase = createClient('https://jqcehtkrvvdvtffgclwa.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxY2VodGtydnZkdnRmZmdjbHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4MTMyNTksImV4cCI6MjA1NDM4OTI1OX0.cuq8t43j7LpSRcxbiWwBJ7LHJ4yAqo1vUuTX9ngyiKk');
+
+module.exports = {supabase};
---
+++ Backend/index.js
@@ -0,0 +1,6 @@
+const dotenv = require("dotenv").config()
+const { supabase } = require("./database");
+
+
+
+console.log(supabase)

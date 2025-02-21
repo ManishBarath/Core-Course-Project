44ead618796950a71afb0a636b50c97fd8c40799
santhoshsparky123 <santhoshsparky333@gmail.com>
2025-02-21 10:41:53 +0530

    upload route added

--- Backend/database.js
+++ Backend/database.js
@@ -1,8 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-console.log(SUPABASE_URL)
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
+// const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
 
 const supabase = createClient('https://jqcehtkrvvdvtffgclwa.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxY2VodGtydnZkdnRmZmdjbHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4MTMyNTksImV4cCI6MjA1NDM4OTI1OX0.cuq8t43j7LpSRcxbiWwBJ7LHJ4yAqo1vUuTX9ngyiKk');
 
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,27 @@
 const dotenv = require("dotenv").config()
 const { supabase } = require("./database");
+console.log("HELLO????????????????????????????????????????????????????")
 
+const express = require("express");
+
+const cors = require('cors')
+
+const app = express();
+const port = 3000;
+
+// Set up Multer for file uploads
+
+
+// Endpoint to handle file uploads
+const router1 = require('./controller/curd')
+
+app.use('/curd',router1)
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
 
 
 console.log(supabase)
